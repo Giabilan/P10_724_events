6d3d292fe5abd3bc0c34f557d5d1694388fba8ad
Rajendran Giabilan <giabilan.r@gmail.com>
2023-09-21 00:43:57 +0200

    Ajout des tests

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -62,7 +62,7 @@ const Page = () => {
         <h2  id="nos-realisations" className="Title">Nos réalisations</h2>
         <EventList />
       </section>
-      <section className="PeoplesContainer">
+      <section className="PeoplesContainer" data-testid="displayPeopleList-testid">
         <h2  id="notre-equipe" className="Title">Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">
@@ -120,9 +120,9 @@ const Page = () => {
         </Modal>
       </div>
     </main>
-    <footer className="row">
+    <footer className="row" data-testid="displayFooter-testid">
       <div className="col presta">
-        <h3>Notre derniére prestation</h3>
+        <h3 data-testid="displayLastEvent-testid">Notre derniére prestation</h3>
         {lastEvent && 
         <EventCard
           imageSrc={lastEvent.cover}
--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,5 +1,7 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import Home from "./index";
+import EventList from "../../containers/Events";
+import EventCard from "../../components/EventCard";
 
 describe("When Form is created", () => {
   it("a list of fields card is displayed", async () => {
@@ -30,15 +32,43 @@ describe("When Form is created", () => {
 
 describe("When a page is created", () => {
   it("a list of events is displayed", () => {
-    // to implement
-  })
+    render(<EventList />);
+    const displayEventList = screen.getByTestId("displayEventList-testid");
+    expect(displayEventList).toBeInTheDocument();
+    screen.findByText("conférence");
+    screen.findByText("AVRIL");
+    screen.findByText("User&Product MixUsers");
+  });
   it("a list a people is displayed", () => {
-    // to implement
-  })
+    render(<Home />);
+    const displayPeopleList = screen.getByTestId("displayPeopleList-testid");
+    expect(displayPeopleList).toBeInTheDocument();
+    screen.findByText("Notre équipe");
+    screen.findByText("Une équipe d’experts dédiés à l’ogranisation de vos événements");
+  });
   it("a footer is displayed", () => {
-    // to implement
-  })
+    render(<Home />);
+    const displayFooter = screen.getByTestId("displayFooter-testid");
+    expect(displayFooter).toBeInTheDocument();
+    screen.findByText("Contactez-nous");
+    screen.findByText("45 avenue de la République, 7000 Paris");
+    screen.findByText("01 23 45 67 89");
+    screen.findByText("contact@77events.com");
+  });
   it("an event card, with the last event, is displayed", () => {
-    // to implement
-  })
+    render(
+      <EventCard
+        title="Conférence #productCON"
+        date={new Date("2022-08-29T20:28:45.744Z")}
+        imageSrc="/images/headway-F2KRf_QfCqw-unsplash.png"
+        label="conférence"
+      />
+    );
+
+    const lastEventTitle = screen.getByText("Conférence #productCON");
+    const lastEventDate = screen.getByText(/août/);
+
+    expect(lastEventTitle).toBeInTheDocument();
+    expect(lastEventDate).toBeInTheDocument();
+  });
 });
